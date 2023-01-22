a7e682fdb49359e21a94dd8e080dbbcf70f7f802
Hazel0106 <118016705+Hazel0106@users.noreply.github.com>
2023-01-22 11:50:45 +0000

    Add files via upload
    Spring Boot back end templates

---
+++ makerspace-backend-springboot/src/main/java/com/makerspace/makerspacebackendspringboot/MakerspaceBackendSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.makerspace.makerspacebackendspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakerspaceBackendSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakerspaceBackendSpringbootApplication.class, args);
+	}
+
+}
---
+++ makerspace-backend-springboot/src/test/java/com/makerspace/makerspacebackendspringboot/MakerspaceBackendSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makerspace.makerspacebackendspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakerspaceBackendSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
