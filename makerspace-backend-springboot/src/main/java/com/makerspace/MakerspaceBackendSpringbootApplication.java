e3cf961c25e8e146021645bbb9c5f8b598560d45
Wanqi <121428454+Wanqi-Strong@users.noreply.github.com>
2023-05-04 09:39:24 +0100

    update db sql

--- makerspace-backend-springboot/src/main/java/com/makerspace/MakerspaceBackendSpringbootApplication.java
+++ makerspace-backend-springboot/src/main/java/com/makerspace/MakerspaceBackendSpringbootApplication.java
@@ -2,12 +2,31 @@ package com.makerspace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
 public class MakerspaceBackendSpringbootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MakerspaceBackendSpringbootApplication.class, args);
+	private static String path;
+
+	public static void main(String[] args) throws UnknownHostException {
+		ConfigurableApplicationContext application =SpringApplication.run(MakerspaceBackendSpringbootApplication.class, args);
+		Environment env = application.getEnvironment();
+		String ip = InetAddress.getLocalHost().getHostAddress();
+		String port = env.getProperty("server.port");
+		String property = env.getProperty("server.servlet.context-path");
+		String ath = property == null ? "" :  property;
+		System.out.println(
+				"\n\t" +
+						"----------------------------------------------------------\n\t" +
+						"Application MakerSpace is running :) Access URLs:\n\t" +
+						"Local: \t\thttp://localhost:" + port + path + "/\n\t" +
+						"External: \thttp://" + ip + ":" + port + path + "/\n\t" +
+						"------------------------------------------------------------");
 	}
 
 }
--- makerspace-backend-springboot/target/classes/schema.sql
+++ makerspace-backend-springboot/target/classes/schema.sql
@@ -8,12 +8,6 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- Schema makerSpace_db
 -- -----------------------------------------------------
 -- This is the db for MakerSpace tables.
-
--- -----------------------------------------------------
--- Schema makerSpace_db
---
--- This is the db for MakerSpace tables.
--- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `makerSpace_db` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin ;
 USE `makerSpace_db` ;
 
@@ -21,28 +15,25 @@ USE `makerSpace_db` ;
 -- Table `makerSpace_db`.`user`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `makerSpace_db`.`user` (
-  `user_id` BIGINT(8) NOT NULL AUTO_INCREMENT,
-  `user_name` VARCHAR(15) NOT NULL,
-  `user_email` VARCHAR(15) NOT NULL,
-  `user_pwd` VARCHAR(40) NOT NULL,
-  `create_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `update_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `status` INT(1) NOT NULL DEFAULT 1 COMMENT 'active-1,inactive-0',
+  `user_id` bigint(8) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(15) COLLATE utf8_bin NOT NULL,
+  `user_email` varchar(40) COLLATE utf8_bin NOT NULL,
+  `user_pwd` varchar(40) COLLATE utf8_bin NOT NULL,
+  `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `status` int(1) NOT NULL DEFAULT '1' COMMENT 'active-1,inactive-0',
   PRIMARY KEY (`user_id`))
 ENGINE = InnoDB;
 
-
 -- -----------------------------------------------------
--- Table `makerSpace_db`.`studio`
+-- Table `makerSpace_db`.`service`
 -- -----------------------------------------------------
-
-
 CREATE TABLE IF NOT EXISTS `makerSpace_db`.`service` (
   `service_id` int(11) NOT NULL AUTO_INCREMENT,
   `service_type` int(1) NOT NULL COMMENT '1 means equipment, 2 means workshop, 3 means studio',
-  `description` varchar(100) COLLATE utf8_bin DEFAULT NULL,
+  `description` varchar(675) COLLATE utf8_bin DEFAULT NULL,
   `status` int(1) NOT NULL DEFAULT '1' COMMENT '1 means available, 0 means unavailable as logical deleted',
-  `picture` blob,
+  `picture` longblob,
   `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `update_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `start_date` timestamp NULL DEFAULT NULL COMMENT 'when service_type equals to 2, this attribute identify workshop start date',
@@ -52,11 +43,31 @@ CREATE TABLE IF NOT EXISTS `makerSpace_db`.`service` (
   `service_name` varchar(40) COLLATE utf8_bin NOT NULL,
   `category` int(1) DEFAULT NULL COMMENT 'when service_type equals to 1, this attribute identify equipment category',
   `active` int(1) NOT NULL DEFAULT '1' COMMENT '1 means available for student, 0 means unavailable.',
-    PRIMARY KEY (`service_id`),
-    FOREIGN KEY (`operator_id`)
-    REFERENCES `makerSpace_db`.`user` (`user_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  PRIMARY KEY (`service_id`),
+  KEY `fk_user` (`operator_id`),
+  CONSTRAINT `fk_user` FOREIGN KEY (`operator_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `makerSpace_db`.`record`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `makerSpace_db`.`record` (
+  `record_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `first_name` varchar(45) COLLATE utf8_bin NOT NULL,
+  `last_name` varchar(45) COLLATE utf8_bin NOT NULL,
+  `reason` varchar(125) COLLATE utf8_bin DEFAULT NULL,
+  `status` int(1) NOT NULL DEFAULT '1' COMMENT '1 means available, 0 means unavailable as logical deleted',
+  `update_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `service_id` int(11) NOT NULL,
+  `student_id` varchar(45) COLLATE utf8_bin NOT NULL COMMENT 'student id or staff id',
+  `start_date` timestamp NULL DEFAULT NULL COMMENT 'for equipment and studio',
+  `end_date` timestamp NULL DEFAULT NULL COMMENT 'for equipment and studio',
+  `state` int(1) NOT NULL DEFAULT '1' COMMENT '1 means pending, 2 means in processing, 3 means complete, 4 means expired',
+  `student_email` varchar(255) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`record_id`),
+  KEY `fk_service` (`service_id`),
+  CONSTRAINT `fk_service` FOREIGN KEY (`service_id`) REFERENCES `services` (`service_id`) ON DELETE NO ACTION ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
