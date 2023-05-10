5ab53af54a29a80d85a5215765657285a7ea1e5c
Wanqi <121428454+Wanqi-Strong@users.noreply.github.com>
2023-05-10 10:26:52 +0100

    update original data

---
+++ Scripts/originalData.sql
@@ -0,0 +1,2 @@
+-- default user, pwd=123456 with encryption
+INSERT INTO `user` VALUES (1,'admin','admin123@gmail.com','$2a$10$ytBLGuoOQsuXQvOLok.mqea5aWEmq978J5KEUfu/kg1.XYE97Dc3S','2023-05-10 09:00:00','2023-05-10 09:00:00',1);
