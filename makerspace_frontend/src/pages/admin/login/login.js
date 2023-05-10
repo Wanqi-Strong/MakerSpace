3fc5d3f2bacc6680dc34cff9ab291192d8dab82b
Wanqi <121428454+Wanqi-Strong@users.noreply.github.com>
2023-05-10 10:19:53 +0100

    update login warm

--- makerspace_frontend/src/pages/admin/login/login.js
+++ makerspace_frontend/src/pages/admin/login/login.js
@@ -1,6 +1,6 @@
 import './login.css';
 
-import React,{useState,useRef} from "react"
+import React, { useState, useRef } from "react"
 import { useNavigate } from "react-router-dom";
 
 import Box from '@mui/material/Box';
@@ -8,7 +8,7 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 
 import CustomAlert from './../../../components/alter/alter'
-function Login(props){
+function Login(props) {
     const [name, setName] = useState('');
     const [pwd, setPwd] = useState('');
     const navigate = useNavigate();
@@ -20,53 +20,54 @@ function Login(props){
 
     async function handleSubmit() {
         let form = {
-            userEmail:name.trim(),
-            userPwd:pwd.trim(),
+            userEmail: name.trim(),
+            userPwd: pwd.trim(),
         }
-        if(!form.userEmail){
+        if (!form.userEmail) {
             nameInput.focus();
             alter.current.showAlert('please input username');
             return;
         }
-        if(!form.userPwd){
+        if (!form.userPwd) {
             pwdInput.focus();
             alter.current.showAlert('please input password');
             return;
         }
-        let res = await React.$req.post(React.$api.userLogin,form);
-        console.log(res);
-        if(res.success){
-            React.$utils.setSessionStorage("userInfo",res.data.data);
+        let res = await React.$req.post(React.$api.userLogin, form);
+        if (res.success) {
+            React.$utils.setSessionStorage("userInfo", res.data.data);
             navigateTo("/admin/home");
+        } else {
+            alter.current.showAlert(res.message);
         }
-      }
+    }
 
-      function navigateTo(url){
-        try{
+    function navigateTo(url) {
+        try {
             navigate(url);
-        }catch(e){
+        } catch (e) {
             throw e;
         }
     }
-    return(
-            <div className="container flex flex_ver flex_center_all">
-                    <Box component="form" noValidate autoComplete="off" sx={{'& .MuiTextField-root': { m: 1, width: '25ch' },}}>   
-                        <div>
-                            <TextField id="username" label="user email" variant="outlined" required size="small"
-                            inputRef={(input) => {setNameInput( input) }} 
-                            value={name} onChange={(event) => {setName(event.target.value);}} />
-                        </div>
-                        <div>
-                            <TextField id="password" label="password" variant="outlined" type="password" required size="small" 
-                            inputRef={(input) => {setPwdInput( input) }} 
-                            value={pwd} onChange={(event) => {setPwd(event.target.value);}} />
-                        </div>
-                        <div className='center-box'>
-                            <Button variant="contained" onClick={handleSubmit} size="small" >Login</Button>
-                        </div>
-                    </Box>
-                <CustomAlert ref={alter} severity='warning' alertTitle='incomplete form'></CustomAlert>
-            </div>
+    return (
+        <div className="container flex flex_ver flex_center_all">
+            <Box component="form" noValidate autoComplete="off" sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' }, }}>
+                <div>
+                    <TextField id="username" label="user email" variant="outlined" required size="small"
+                        inputRef={(input) => { setNameInput(input) }}
+                        value={name} onChange={(event) => { setName(event.target.value); }} />
+                </div>
+                <div>
+                    <TextField id="password" label="password" variant="outlined" type="password" required size="small"
+                        inputRef={(input) => { setPwdInput(input) }}
+                        value={pwd} onChange={(event) => { setPwd(event.target.value); }} />
+                </div>
+                <div className='center-box'>
+                    <Button variant="contained" onClick={handleSubmit} size="small" >Login</Button>
+                </div>
+            </Box>
+            <CustomAlert ref={alter} severity='error' alertTitle='Login Failed'></CustomAlert>
+        </div>
     );
 }
 export default Login;
